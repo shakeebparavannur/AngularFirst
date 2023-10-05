4a21f523754481aa922d6a03a45752ac34ea24b1
Shakeeb <shakeebparavannur@gmail.com>
2023-10-05 23:35:17 +0530

    begin

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -9,7 +9,9 @@ export class TaskComponent {
   task='initial data';
   name='';
   selectedOption='option1'
+  newTaskAdded = false;
     addTask(e:any){
+      
       console.log(e.target.value);
       this.task = e.target.value;
     }
